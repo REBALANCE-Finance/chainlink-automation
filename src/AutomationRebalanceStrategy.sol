d4bb9c297c2fd042f8b04b5568239a6a20bba438
sapph1re <dev.romanv@gmail.com>
2024-06-17 20:55:25 -0600

    rebalance settings: min interval, min rate delta

--- src/AutomationRebalanceStrategy.sol
+++ src/AutomationRebalanceStrategy.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {AutomationCompatibleInterface} from "chainlink/v0.8/automation/AutomationCompatible.sol";
 import {IProvider} from "./interfaces/IProvider.sol";
 import {IInterestVault} from "./interfaces/IInterestVault.sol";
@@ -8,26 +9,47 @@ import {IRebalancerManager} from "./interfaces/IRebalancerManager.sol";
 
 // This contract implements a rebalancing strategy for an interest vault.
 // It is designed to be integrated with Chainlink Automation.
-contract RebalanceStrategy is AutomationCompatibleInterface {
+contract RebalanceStrategy is AutomationCompatibleInterface, Ownable {
     // The interest vault to manage
     IInterestVault public vault;
     // The rebalancer manager responsible for the rebalancing process
     IRebalancerManager public rebalancerManager;
     // The address of Chainlink's Forwarder contract that will be used to trigger performUpkeep()
     address public forwarder;
+    // The minimal rebalance interval in seconds
+    uint256 public minRebalanceInterval;
+    // The minimal interest rate delta to trigger a rebalance, in ray units
+    uint256 public minRebalanceDeltaRate;
+    // The last time rebalance was performed
+    uint256 public lastRebalance;
 
-    // Event emitted when rebalance is performed
+    // When rebalance is performed
     event UpkeepPerformed(IProvider newProvider);
+    // When forwarder is set
+    event ForwarderSet(address indexed forwarder);
+    // When rebalance settings are updated
+    event SettingsUpdated(uint256 minRebalanceInterval, uint256 minRebalanceDeltaRate);
 
-    constructor(IInterestVault _vault, IRebalancerManager _rebalancerManager) {
+
+    constructor(IInterestVault _vault, IRebalancerManager _rebalancerManager) Ownable(msg.sender) {
         vault = _vault;
         rebalancerManager = _rebalancerManager;
+        // default rebalance settings
+        minRebalanceInterval = 600;           // 10 minutes interval
+        minRebalanceDeltaRate = 10**25;       // 1% rate delta
     }
 
-    // Called only once when Chainlink Upkeep is set up
-    function setForwarder(address _forwarder) external {
-        require(forwarder == address(0), "Forwarder already set");
+    // Set the address of Chainlink's Forwarder contract that will be able to trigger rebalance
+    function setForwarder(address _forwarder) external onlyOwner {
         forwarder = _forwarder;
+        emit ForwarderSet(_forwarder);
+    }
+
+    // Update the rebalance settings
+    function updateSettings(uint256 _minRebalanceInterval, uint256 _minRebalanceDeltaRate) external onlyOwner {
+        minRebalanceInterval = _minRebalanceInterval;
+        minRebalanceDeltaRate = _minRebalanceDeltaRate;
+        emit SettingsUpdated(_minRebalanceInterval, _minRebalanceDeltaRate);
     }
 
     // Function called by Chainlink to perform the upkeep, i.e. rebalance the vault
@@ -35,6 +57,7 @@ contract RebalanceStrategy is AutomationCompatibleInterface {
         require(msg.sender == forwarder, "Only the forwarder can call this function");
         IProvider newProvider = abi.decode(performData, (IProvider));
         rebalancerManager.rebalanceVault(vault, type(uint256).max, vault.activeProvider(), newProvider, 0, true);
+        lastRebalance = block.timestamp;
         emit UpkeepPerformed(newProvider);
     }
 
@@ -57,10 +80,16 @@ contract RebalanceStrategy is AutomationCompatibleInterface {
     // Function to check whether a rebalancing is needed
     // It aims to rebalance to another provider as soon as its deposit rate is the highest
     function shouldRebalance() public view returns (bool should, IProvider newProvider) {
+        // limit the rebalance frequency
+        if (block.timestamp - lastRebalance < minRebalanceInterval) {
+            return (false, vault.activeProvider());
+        }
+        // get providers and rates
         IProvider[] memory providers = vault.getProviders();
         uint256[] memory rates = depositRates();
         IProvider activeProvider = vault.activeProvider();
         uint256 activeRate = activeProvider.getDepositRateFor(vault);
+        // find the best provider
         uint256 highestRate = 0;
         IProvider bestProvider;
         for (uint256 i = 0; i < rates.length; i++) {
@@ -69,7 +98,9 @@ contract RebalanceStrategy is AutomationCompatibleInterface {
                 bestProvider = providers[i];
             }
         }
-        if (activeRate != highestRate) {
+        // we should rebalance if the active provider rate is lower than the best provider rate
+        // by at least minRebalanceDeltaRate
+        if (highestRate - activeRate >= minRebalanceDeltaRate) {
             should = true;
             newProvider = bestProvider;
         } else {
